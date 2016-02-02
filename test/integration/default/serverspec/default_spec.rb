f4d79a91949737a194796798ea64fd0307a2bf26
Josh Beauregard <josh.beauregard@knectar.com>
2016-02-02 15:45:39 -0500

    ADD. created basic tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-describe 'chef_cookbook_starter::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+services = %w(mysql php-fpm nginx)
+
+services.each do |s|
+  describe service(s) do
+    it { should be_enabled }
   end
+end
+
+describe user('omeka_web') do
+  it { should should exist }
+  it { should belong_to_group 'www-data' }
 end

d8c89113af66ab438293e632d79e5a7350f9d92c
Josh Beauregard <josh.beauregard@knectar.com>
2016-02-02 18:07:59 -0500

    ADD zip file download

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,29 @@
 #
-# Cookbook Name:: chef_cookbook_starter
+# Cookbook Name:: omeka
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Harvard ATG, All Rights Reserved.
+#
+
+user node['omeka']['user'] do
+  action :create
+  comment 'Omeka User'
+end
+
+directory node['omeka']['location'] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+
+  action :create
+end
+
+remote_file '/tmp/omeka.zip' do
+  owner node['omeka']['user']
+  group node['nginx']['user']
+  mode '0644'
+  source 'http://www.example.com/remote_file'
+  checksum 'sha256checksum'
+end
+
+

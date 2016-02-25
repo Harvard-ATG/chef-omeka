bafbeb8cb5f1f9b844cebaca957f428f2ca81608
Josh Beauregard <josh.beauregard@knectar.com>
2016-02-25 13:16:56 -0500

    major refactoring

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,54 +1,35 @@
-# Takes a file and or a url decides if it is a git repo or an archive. downoads or clones it.
+# Takes a file and or a url decides if it is a git repo or an archive.
+# Downloads or clones it. to the destination.
+# Do not enter url with trailing slash.
 def get_files(url, file, destination)
-  # "temp_file = "#{Chef::Config['file_cache_path'] || '/tmp'}/#{file}.#{extension[1]}"
-
-  case file.=~(/\.([\w\.]*$)/)
-  when 'tar.gz'
-    puts 'This is a tar gzip!!'
-
-    file_arch = "#{Chef::Config['file_cache_path'] || '/tmp'}/#{file}"
-    remote_file file_arch do
-      owner 'root'
-      group 'root'
-      mode '0644'
-      source "#{url}/#{file}"
-      not_if File.readable?(file)
-    end
-    bash "Untar #{file}" do
-      cwd ::File.dirname(file_arch)
-      code <<-EOH
-        tar-xC #{destination} -f #{file};
-      EOH
-      not_if { ::File.readable(file) }
-    end
-  when 'zip'
-    puts 'This is a zip!!'
-    file_arch = "#{Chef::Config['file_cache_path'] || '/tmp'}/#{file}"
-    remote_file file_arch do
-      owner 'root'
-      group 'root'
-      mode '0644'
-      source "#{url}/#{file}"
-      not_if File.readable?(file)
-    end
-    bash "Unzip #{file}" do
-      cwd ::File.dirname(file_arch)
-      code <<-EOH
-        unzip -d #{destination} -qo #{file};
-      EOH
-      not_if { ::File.readable(file) }
-    end
-
-  when 'git'
-    puts 'This is a git repo!!'
+  case File.extname(file)
+  when '.tar.gz', '.zip' then unpack_archive(url, file, destination)
+  when '.git'
     git 'destination' do
       repository "#{url}/#{file}"
       reference 'master'
       user node['apache']['user']
       action :sync
     end
+  else fail ArgumentError, "dunno how to handle #{file}"
+  end
+end
 
-  else
-    puts 'This is something else!'
+def unpack_archive(url, file, destination)
+  remote_file "#{Chef::Config['file_cache_path'] || '/tmp'}/#{file}" do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source "#{url}/#{file}"
+    not_if File.readable?(file)
+  end
+  extract = case File.extname(file)
+            when '.tar.gz' then "tar-xC #{destination} -f #{file};"
+            when 'zip' then "unzip -d #{destination} -qo #{file};"
+            end
+  bash "Extract #{file}" do
+    cwd ::File.dirname("#{Chef::Config['file_cache_path'] || '/tmp'}/#{file}")
+    code   extract
+    not_if { ::File.readable(file) }
   end
 end

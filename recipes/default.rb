cb859bffbef92986f098ae08f8daecc0a3c3b5ff
Josh Beauregard <josh_beauregard@harvard.edu>
2016-05-09 12:30:02 -0400

    fixed npm issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ case node['platform_family']
 when 'rhel', 'fedora'
   packages.push('zlib-devel', 'httpd-devel', 'pcre', 'pcre-devel', 'php-mysql', 'php-gd')
 when 'debian'
-  packages.push('php5-memcache', 'php5-gd', 'php5-mysql')
+  packages.push('php5-memcache', 'php5-gd', 'php5-mysql', 'apache2-mpm-prefork')
 end
 packages.each do |p|
   package p do
